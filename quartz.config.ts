994224aae4576ace5e0f79c611a7a6dd822e5e3d
QueNerguy <voskuilenquinte@gmail.com>
2025-08-08 17:53:43 +0200

    Quartz sync: Aug 8, 2025, 5:53 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -32,7 +32,7 @@ const config: QuartzConfig = {
           light: "#fcf5e4",
           lightgray: "#e4dac2",
           gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
+          darkgray: "#000000",
           dark: "#2b2b2b",
           secondary: "#284b63",
           tertiary: "#84a59d",
